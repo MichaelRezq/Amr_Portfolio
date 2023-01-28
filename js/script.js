738002aaaf8e39a24743dbaed5a5c9cd07c7b72c
Michael Rezq <michaelrezq9@gmail.com>
2023-01-28 04:19:10 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 // ---------------handling the up button-----------------------------------
-// let upButton = document.querySelector(".up");
-// // if the window hight is > 100vh display the up button else display none
-// window.onscroll = () => {
-//   if (window.scrollY >= window.innerHeight) {
-//     upButton.style.display = "flex";
-//   } else {
-//     upButton.style.display = "none";
-//   }
-// };
-// function handleUp() {
-//   console.log("clicked");
-//   window.scroll(0, 0);
-// }
+let upButton = document.querySelector(".up");
+// if the window hight is > 100vh display the up button else display none
+window.onscroll = () => {
+  if (window.scrollY >= window.innerHeight) {
+    upButton.style.display = "flex";
+  } else {
+    upButton.style.display = "none";
+  }
+};
+function handleUp() {
+  console.log("clicked");
+  window.scroll(0, 0);
+}
 // -----------------------handling the dark and white mode--------------------
 let swapIcon = document.querySelector(".swapIcon");
 function hanleDarkAndWhite() {
@@ -34,45 +34,32 @@ function hanleDarkAndWhite() {
 }
 
 // -------------------- when the page reloaded --------------------------
-// let screenReload = setTimeout(loadingAnimationFunction, 2000);
-// let loadingAnimation = document.querySelector(".loading");
-// let websiteContent = document.querySelector(".websiteContent");
-// console.log(loadingAnimation);
 
-// function loadingAnimationFunction() {
-//   // display the page content and hide loading animation
-//   websiteContent.style.display = "block";
-//   loadingAnimation.style.display = "none";
-// }
+let screenReload = setTimeout(loadingAnimationFunction, 2000);
+let loadingAnimation = document.querySelector(".loading");
+let websiteContent = document.querySelector(".websiteContent");
 
-//
-
-// console.log(allSections);
-// window.onscroll = () => {
-//   allSections.forEach((section) => {
-//     if (section.offsetHeight === section.offsetTop) {
-//       console.log(section);
-//     }
-//   });
-// };
-
-//
+function loadingAnimationFunction() {
+  // display the page content and hide loading animation
+  websiteContent.style.display = "block";
+  loadingAnimation.style.display = "none";
+}
 
 // adding animation for each section when the scrollY
-// let allSections = document.querySelectorAll("section");
-// let proficiency = document.querySelector(".proficiency");
-// function scrollActive() {
-//   allSections.forEach((section) => {
-//     let a = document.querySelector(`#${section.id}Icon a`);
-//     if (
-//       window.pageYOffset > section.offsetTop - 58 &&
-//       window.pageYOffset <= section.offsetTop - 58 + section.offsetHeight
-//     ) {
-//       // console.log(section.getAttribute("id"));
-//       a.classList.add("active--link");
-//     } else {
-//       a.classList.remove("active--link");
-//     }
-//   });
-// }
-// window.addEventListener("scroll", scrollActive);
+let allSections = document.querySelectorAll("section");
+let proficiency = document.querySelector(".proficiency");
+function scrollActive() {
+  allSections.forEach((section) => {
+    let a = document.querySelector(`#${section.id}Icon a`);
+    if (
+      window.pageYOffset > section.offsetTop - 58 &&
+      window.pageYOffset <= section.offsetTop - 58 + section.offsetHeight
+    ) {
+      // console.log(section.getAttribute("id"));
+      a.classList.add("active--link");
+    } else {
+      a.classList.remove("active--link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
